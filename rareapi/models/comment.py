37fdd0102ead482b2feaefd4e7e189ef02ec4d67
Jacob-Folley <jakebfolley@gmail.com>
2022-02-15 15:05:02 -0600

    save

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Comment(models.Model):
-    user_id = models.ForeignKey("theUser", on_delete=models.CASCADE)
+    user = models.ForeignKey("theUser", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
     text = models.TextField()
     date = models.DateTimeField(auto_now=True)
